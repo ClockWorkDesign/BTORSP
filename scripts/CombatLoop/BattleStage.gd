70db6a5f2e5acffa48db29fd4516eb57979e041b
ClockWorkDesign <clockworkdesignwork@gmail.com>
2024-06-06 21:45:48 -0400

    started work on winning battle scene
    need to be able to tell if the enemies are still alive or not

--- scripts/CombatLoop/BattleStage.gd
+++ scripts/CombatLoop/BattleStage.gd
@@ -39,7 +39,7 @@ func _ready():
 	startTurn()
 	
 
-
+# this is what happens when the 'end turn' button is pressed
 func _on_end_turn_pressed():
 	
 	var shells = %characters.get_children()
@@ -53,6 +53,25 @@ func _on_end_turn_pressed():
 		shells.pop_front()
 		
 	
+<<<<<<< Updated upstream
+=======
+	for piece in getEveryone():
+		piece.onTurnEnd()
+	
+	for piece in enemyPieces: if piece.dead: enemyPieces.erase(piece)
+	for piece in playerPieces: if piece.dead: playerPieces.erase(piece)
+	
+	if enemyPieces.size() == 0: endBattle("W")
+	print(enemyPieces.size())
+	if playerPieces.size() == 0: endBattle('')
+	
+	
+	
+	
+	# need to check if all enemies are dead, that should be done after attacks tho
+	startTurn()
+	
+>>>>>>> Stashed changes
 
 func startTurn():
 	
@@ -69,6 +88,17 @@ func sort_ascending(a : GamePieceShell, b):
 		return true
 	return false
 
+<<<<<<< Updated upstream
 
+=======
+func getEveryone():
+	var everyone : Array = enemyPieces.duplicate()
+	everyone.append_array(playerPieces)
+	return everyone
+	
+func endBattle(outcome : String):
+	if outcome == "W": print("win")
+	else: print("loose")
+>>>>>>> Stashed changes
 
 
