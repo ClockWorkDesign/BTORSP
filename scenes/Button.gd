b0484c54b293283f74a4c854a8f879082a443f4e
ToraNopon <Toranopon@gmail.com>
2024-06-13 10:19:23 -0400

    added files and begun to add scene swapping function

---
+++ scenes/Button.gd
@@ -0,0 +1,17 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func on_Button_pressed() -> void:
+	get_tree().change_scene()
+
+func _on_pressed():
+	pass # Replace with function body.
